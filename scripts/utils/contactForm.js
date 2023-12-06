3743f4b9f9737c3d89075e83c80ffc2c9f673b10
Allan Lannoy <allan1411lannoy@gmail.com>
2023-12-06 17:20:54 +0100

    Correction modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -9,22 +9,15 @@ function closeModal() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Sélectionnez le formulaire et le bouton Envoyer
     const contactForm = document.querySelector('form');
     const sendButton = contactForm.querySelector('.contact_button');
 
-    // Ajoutez un gestionnaire d'événements au clic sur le bouton Envoyer
     sendButton.addEventListener('click', function (e) {
-        // Empêchez le comportement par défaut du formulaire (rechargement de la page)
         e.preventDefault();
-
-        // Sélectionnez tous les champs de saisie du formulaire
         const formInputs = contactForm.querySelectorAll('.text-control');
 
-        // Créez un objet pour stocker les données du formulaire
         const formData = {};
 
-        // Parcourez chaque champ de saisie
         formInputs.forEach(input => {
             // Récupérez l'ID et la valeur de chaque champ de saisie
             const inputId = input.id;
@@ -32,10 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Stockez la valeur dans l'objet formData avec le nom du champ comme clé
             formData[inputId] = inputValue;
+            closeModal()
         });
 
         // Affichez les données du formulaire dans la console
         console.log('Données du formulaire :', formData);
-
     });
 });
